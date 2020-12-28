3f721270e8c4aa2ed9653a5d206da374e5713adf
Jim Lin <b04705003@ntu.edu.tw>
2020-12-28 12:22:12 +0800

    add public IP related iptables rules

--- scripts/add_dest_nat_iptables_rules.sh
+++ scripts/add_dest_nat_iptables_rules.sh
@@ -2,3 +2,5 @@
 
 # NAT it when the destination IP is 10.15.20.25 (our hello world server)
 iptables -t nat -A POSTROUTING -d 10.15.20.25/32 -o eth0 -j MASQUERADE
+# NAT it when the destination IP is 10.15.20.1 (use eth0 to NAT the incoming packets)
+iptables -t nat -A POSTROUTING -d 10.15.20.1/32 -o eth0 -j MASQUERADE
